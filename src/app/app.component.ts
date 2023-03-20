d3aea47a978340d049f7e9380c5921612ea3084e
mihail-soltan <mihail.soltan@protonmail.ch>
2023-03-20 19:32:36 +0200

    add manage works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'veziv-it-frontend';
+  isHamburgerOpen: boolean = false;
+  user: string = 'Veziv IT';
+
+  navItems: any[] = [
+    { name: 'PORTFOLIO', link: '/works' },
+    { name: 'MANAGE PORTFOLIO', link: '/manage' },
+  ];
+  constructor(private router: Router) {}
+  
+  onNavItemClick(item: string){
+    this.isHamburgerOpen = false;
+    this.router.navigate([item]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { WorkListComponent } from './components/work-list/work-list.component';
+import { ManageWorksComponent } from './components/manage-works/manage-works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkListComponent,
+    ManageWorksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
